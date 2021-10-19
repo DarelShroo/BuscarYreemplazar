6864a7f5bfb2e77c944fb71607620d6645216ad4
Darel M <cls18drl@gmail.com>
2021-10-19 16:44:11 +0100

    end

--- src/main/java/dad/buscaryreemplazar/BuscarYreemplazar.java
+++ src/main/java/dad/buscaryreemplazar/BuscarYreemplazar.java
@@ -1,19 +1,39 @@
 package dad.buscaryreemplazar;
 
 import javafx.application.Application;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class BuscarYreemplazar extends Application {
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage stage) throws IOException {
+        GridPane gridpane = new GridPane();
+        ColumnConstraints column1 = new ColumnConstraints();
+        column1.setPercentWidth(20);
+        ColumnConstraints column2 = new ColumnConstraints();
+        column2.setPercentWidth(30);
+        ColumnConstraints column3 = new ColumnConstraints();
+        column3.setPercentWidth(30);
+        ColumnConstraints column4 = new ColumnConstraints();
+        column4.setPercentWidth(20);
+
+
+        gridpane.getColumnConstraints().addAll(column1, column2, column3, column4);
+        gridpane.setHgap(10);
+        gridpane.setVgap(5);
+        gridpane.setPadding(new Insets(0, 10, 0, 10));
+        column4.setHalignment(HPos.RIGHT);
         Label buscarLabel = new Label("Buscar:");
         Label reemplazarLabel = new Label("Reemplazar con:");
         TextField buscarTextField = new TextField();
@@ -25,58 +45,48 @@ public class BuscarYreemplazar extends Application {
         Button buttonReemplazarTodo = new Button("Reemplazar todo");
         Button buttonCerrar = new Button("Cerrar");
         Button buttonAyuda = new Button("Ayuda");
-        GridPane gridPane = new GridPane();
         CheckBox checkBoxMayusMinus = new CheckBox("Mayúsculas y minúsculas");
         CheckBox checkBoxExpr = new CheckBox("Expresión regular");
         CheckBox checkBoxBuscarAtras = new CheckBox("Buscar hacia atrás");
         CheckBox checkBoxResResult = new CheckBox("Resaltar resultados");
 
-        gridPane.setHgap(6);
-        gridPane.setVgap(6);
-        gridPane.setPadding(new Insets(6));
-
         //Primera Fila
-        gridPane.add(buscarLabel, 0, 0);
+        gridpane.add(buscarLabel, 0, 0);
         // node, columnIndex, rowIndex, columnSpan, rowSpan:
-        gridPane.add(buscarTextField, 1, 0, 2, 1);
-        gridPane.add(buttonBuscar, 3, 0);
+        gridpane.add(buscarTextField, 1, 0, 2, 1);
+        gridpane.add(buttonBuscar, 3, 0, 1, 1);
 
         //Segunda Fila
-        gridPane.add(reemplazarLabel, 0, 1);
-        gridPane.add(reemplazarTextField, 1, 1, 2, 1);
-        gridPane.add(buttonReemplazar, 3, 1);
+        gridpane.add(reemplazarLabel, 0, 1);
+        gridpane.add(reemplazarTextField, 1, 1, 2, 1);
+        gridpane.add(buttonReemplazar, 3, 1);
 
         //Tercera Fila
-        gridPane.add(checkBoxMayusMinus, 1, 2);
-        gridPane.add(checkBoxBuscarAtras, 2, 2);
-        gridPane.add(buttonReemplazarTodo, 3, 2);
+        gridpane.add(checkBoxMayusMinus, 1, 2);
+        gridpane.add(checkBoxBuscarAtras, 2, 2);
+        gridpane.add(buttonReemplazarTodo, 3, 2);
 
-        //Cuarta Fila
-        gridPane.add(checkBoxExpr, 1, 3);
-        gridPane.add(checkBoxResResult, 2, 3);
+        ///Cuarta Fila
+        gridpane.add(checkBoxExpr, 1, 3);
+        gridpane.add(checkBoxResResult, 2, 3);
 
         //Quinta fila
-        gridPane.add(buttonCerrar, 3, 3);
+        gridpane.add(buttonCerrar, 3, 3);
 
         //Sexta Fila
-        gridPane.add(buttonAyuda, 3, 4);
+        gridpane.add(buttonAyuda, 3, 4);
 
-        //TexField min-width
-        reemplazarTextField.setMinWidth(280);
-        buscarTextField.setMinWidth(280);
+        //Buttons width
+        buttonBuscar.setPrefWidth(110);
+        buttonReemplazar.setPrefWidth(110);
+        buttonReemplazarTodo.setPrefWidth(110);
+        buttonCerrar.setPrefWidth(110);
+        buttonCerrar.setPrefWidth(110);
+        buttonAyuda.setPrefWidth(110);
 
-        //Buttons min-width
-        buttonBuscar.setMinWidth(110);
-        buttonReemplazar.setMinWidth(110);
-        buttonReemplazarTodo.setMinWidth(110);
-        buttonCerrar.setMinWidth(110);
-        buttonCerrar.setMinWidth(110);
-        buttonAyuda.setMinWidth(110);
-
-        Scene myScene = new Scene(gridPane, 500, 160);
-        primaryStage.setScene(myScene);
-        primaryStage.setTitle("Buscar y Reemplazar");
-        primaryStage.centerOnScreen();
-        primaryStage.show();
+        Scene scene = new Scene(gridpane, 600, 150);
+        stage.setTitle("Buscar y reemplazar");
+        stage.setScene(scene);
+        stage.show();
     }
 }
