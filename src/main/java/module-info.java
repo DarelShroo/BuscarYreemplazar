182381bfd70df720ff865ceb53625f7fa9ccf4bf
Darel M <cls18drl@gmail.com>
2021-10-27 21:58:21 +0100

    pom y constructor corregido

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.dad.buscaryreemplazar {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-
-    opens com.dad.buscaryreemplazar to javafx.fxml;
-    exports dad.buscaryreemplazar;
-}
